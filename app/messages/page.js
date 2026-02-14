91ded21cfaa5a98bb047dd7988ce14a3e1a9da07
Dilyannn <122313669+Dilyannn@users.noreply.github.com>
2026-02-14 20:11:49 +0200

    fix: correct fetch config key to next for ISR
    This enables Incremental Static Regeneration (ISR) by correctly passing the revalidate property to the Next.js extended fetch API.

--- app/messages/page.js
+++ app/messages/page.js
@@ -2,10 +2,12 @@ import Messages from '@/components/messages';
 
 export default async function MessagesPage() {
   const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
+    //// cache: 'no-store', //~ disable caching for this request; default in  next.js 15^
+    next: {
+      revalidate: 10 //~ revalidate this data every 10 seconds;
     },
   });
+
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
